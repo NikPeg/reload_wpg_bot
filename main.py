f38bd491eb393690baada8d2efa9f8ab4db518cc
bebra228hacer <104512379+bebra228hacer@users.noreply.github.com>
2025-01-26 22:00:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import telebot
 from telebot import types
 import datetime
 import time
+import schedule
+import datetime
+import threading
+
 
 with open("messages.json", "r", encoding='utf-8') as messages:
     txt = json.load(messages)
@@ -12,6 +16,20 @@ with open("config.json", "r", encoding='utf-8') as config:
 bot = telebot.TeleBot(config_bd["tg_token"])
 openai.api_key = config_bd["chatGPT_token"]
 
+
+class MockMessage:
+    def __init__(self, chat_id, text):
+        self.chat = MockChat(chat_id)
+        self.text = text
+    def __repr__(self):
+        return f"MockMessage(chat_id={self.chat.id}, text='{self.text}')"
+
+
+class MockChat:
+    def __init__(self, chat_id):
+        self.id = chat_id
+    def __repr__(self):
+        return f"MockChat(id={self.id})"
 
 def user_get_thread(message):
     with open("user.json", 'r+', encoding='utf-8') as user:
@@ -39,7 +57,7 @@ def user_requests_upgrade(message):
             req = 100500
         else:
             print("Плохо мне")
-        for req_time in data[id]["req"][:]: #копируем массив чтобы не было ошибки
+        for req_time in data[id]["req"][:]: 
             try:
                 datetime.datetime.fromisoformat(req_time)
             except (ValueError, TypeError):
@@ -80,17 +98,18 @@ def user_data_upgrade(message, key:str = "str please", value:str = "str please")
                 elif data[value] != 0:
                     bot.send_message(message.chat.id, txt["msg"]["country_choose2_busy"].format(country=value))
                     return "busy somebody"
-                else:
-                    for country in txt["btn"]["country"]:
-                        if data[country] == id:
-                            data[country] = 0
-                            data[value] = id
-                            data[id][key] = value
-                            user.seek(0)
-                            json.dump(data, user, indent=4, ensure_ascii=False)
-                            user.truncate()
-                            bot.send_message(message.chat.id, text = txt["msg"]["country_choose2"].format(country=str(message.text)))
-                            return "success"
+                for country in txt["btn"]["country"]:
+                    
+                    if data[country] == id:
+                        data[country] = 0
+                        data[value] = id
+                        data[id]["country"] = value
+                        user.seek(0)
+                        json.dump(data, user, indent=4, ensure_ascii=False)
+                        user.truncate()
+                        bot.send_message(message.chat.id, text = txt["msg"]["country_choose2"].format(country=str(message.text)))
+                        return "success"
+                    
             if key == "sub_lvl":
                 data[id][key] = value
                 user.seek(0)
@@ -105,6 +124,9 @@ def user_data_upgrade(message, key:str = "str please", value:str = "str please")
                 user.truncate()
                 return "successs"
         else:
+            if data[value] != 0:
+                bot.send_message(message.chat.id, txt["msg"]["country_choose2_busy"].format(country=value))
+                return "busy somebody"
             data[id] = {
                 "nickname":str(message.from_user.username),
                 "id_thread":"None",
@@ -112,6 +134,7 @@ def user_data_upgrade(message, key:str = "str please", value:str = "str please")
                 "sub_lvl":"0",
                 "req":"0"
             }
+            data[value] = id
             user.seek(0)
             json.dump(data, user, indent=4, ensure_ascii=False)
             user.truncate()
@@ -121,9 +144,6 @@ def chat_gpt(message):
     with open("user.json", 'r+', encoding='utf-8') as user:
         data = json.load(user)
     user_id = str(message.chat.id)
-    if user_id not in data or "country" not in data[user_id] or "id_thread" not in data[user_id]:
-        bot.send_message(message.chat.id, text = "Произошла ошибка, пожалуйста, перезапустите бота.")
-        return
     user_country = data[user_id]["country"]
     assist_id = config_bd[user_country]
     thread_id = data[user_id]["id_thread"]
@@ -160,6 +180,54 @@ def chat_gpt(message):
                 markup.row(btn4, btn5, btn6)
                 bot.send_message(message.chat.id, text=content_item.text.value,reply_markup=markup)
 
+@bot.message_handler(commands=['answer'])
+def handle_answer(message):
+    with open("user.json", "r", encoding='utf-8') as user:
+        user = json.load(user)
+    if str(message.chat.id) in user["admin_list"]:
+        markup = types.ReplyKeyboardMarkup()
+        btn1 = types.KeyboardButton(text=txt["btn"]["country"][0])
+        btn2 = types.KeyboardButton(text=txt["btn"]["country"][1])
+        btn3 = types.KeyboardButton(text=txt["btn"]["country"][2])
+        markup.row(btn1, btn2, btn3)
+        btn4 = types.KeyboardButton(text=txt["btn"]["country"][3])
+        btn5 = types.KeyboardButton(text=txt["btn"]["country"][4])
+        btn6 = types.KeyboardButton(text=txt["btn"]["country"][5])
+        markup.row(btn4, btn5, btn6)
+        btn7 = types.KeyboardButton(text=txt["btn"]["country"][6])
+        btn8 = types.KeyboardButton(text=txt["btn"]["country"][7])
+        btn9 = types.KeyboardButton(text=txt["btn"]["country"][8])
+        markup.row(btn7, btn8, btn9)
+        bot.send_message(message.chat.id, text = txt["msg"]["mail"], reply_markup=markup)
+        bot.register_next_step_handler(message, send_admin_mail)
+def send_admin_mail(message):
+    if str(message.text) in txt["btn"]["country"]:
+        with open("user.json", 'r+', encoding='utf-8') as user:
+            data = json.load(user)
+        bot.send_message(message.chat.id, text = txt["msg"]["mail_text"])
+        bot.register_next_step_handler(message, send_admin_mail1, id=data[str(message.text)])
+    else:
+        bot.send_message(message.chat.id, text = txt["msg"]["country_choose2_er"])
+        bot.register_next_step_handler(message, send_admin_mail)
+def send_admin_mail1(message, id):
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton(text=txt["btn"]["main_win"][0])
+    btn2 = types.KeyboardButton(text=txt["btn"]["main_win"][1])
+    btn3 = types.KeyboardButton(text=txt["btn"]["main_win"][2])
+    markup.row(btn1, btn2, btn3)
+    btn4 = types.KeyboardButton(text=txt["btn"]["main_win"][3])
+    btn5 = types.KeyboardButton(text=txt["btn"]["main_win"][4])
+    btn6 = types.KeyboardButton(text=txt["btn"]["main_win"][5])
+    markup.row(btn4, btn5, btn6)
+    bot.send_message(message.chat.id, text = txt["msg"]["mail_done"], reply_markup=markup)
+    try:
+        with open("user.json", "r", encoding='utf-8') as user:
+            user = json.load(user)
+        bot.send_message(id, text = txt["msg"]["admin_mail_to"].format(text = message.text)) 
+    except ValueError:
+        pass
+    bot.register_next_step_handler(message, main_win1)
+
 
 @bot.message_handler(func=lambda message: True)
 def main_win(message):
@@ -192,8 +260,27 @@ def main_win(message):
 def main_win1(message):
     if str(message.text) in txt["btn"]["main_win"]:
         if message.text == str(txt["btn"]["main_win"][0]):
-            bot.send_message(message.chat.id, text = "Да не сделал я еще")
-            bot.register_next_step_handler(message, main_win1)
+            with open("user.json", "r", encoding='utf-8') as user:
+                user = json.load(user)
+            if user[str(message.chat.id)]["sub_lvl"] == 1:
+                bot.send_message(str(message.chat.id), text = txt["msg"]["mail_err"])
+                bot.register_next_step_handler(message, main_win1)
+            else:
+                markup = types.ReplyKeyboardMarkup()
+                btn1 = types.KeyboardButton(text=txt["btn"]["country"][0])
+                btn2 = types.KeyboardButton(text=txt["btn"]["country"][1])
+                btn3 = types.KeyboardButton(text=txt["btn"]["country"][2])
+                markup.row(btn1, btn2, btn3)
+                btn4 = types.KeyboardButton(text=txt["btn"]["country"][3])
+                btn5 = types.KeyboardButton(text=txt["btn"]["country"][4])
+                btn6 = types.KeyboardButton(text=txt["btn"]["country"][5])
+                markup.row(btn4, btn5, btn6)
+                btn7 = types.KeyboardButton(text=txt["btn"]["country"][6])
+                btn8 = types.KeyboardButton(text=txt["btn"]["country"][7])
+                btn9 = types.KeyboardButton(text=txt["btn"]["country"][8])
+                markup.row(btn7, btn8, btn9)
+                bot.send_message(message.chat.id, text = txt["msg"]["mail"], reply_markup=markup)
+                bot.register_next_step_handler(message, send_mail)
         if message.text == str(txt["btn"]["main_win"][1]):
             with open("map.png", 'rb') as photo:
                 bot.send_photo(message.chat.id, photo)
@@ -232,6 +319,26 @@ def main_win1(message):
                 markup.add(btn1, btn2, btn3)
                 bot.send_photo(message.chat.id, photo, reply_markup=markup)
             bot.register_next_step_handler(message, sub)
+    elif str(message.text) in txt["btn"]["admin_commands"]:
+        if message.text == "answer":
+            with open("user.json", "r", encoding='utf-8') as user:
+                user = json.load(user)
+            if str(message.chat.id) in user["admin_list"]:
+                markup = types.ReplyKeyboardMarkup()
+                btn1 = types.KeyboardButton(text=txt["btn"]["country"][0])
+                btn2 = types.KeyboardButton(text=txt["btn"]["country"][1])
+                btn3 = types.KeyboardButton(text=txt["btn"]["country"][2])
+                markup.row(btn1, btn2, btn3)
+                btn4 = types.KeyboardButton(text=txt["btn"]["country"][3])
+                btn5 = types.KeyboardButton(text=txt["btn"]["country"][4])
+                btn6 = types.KeyboardButton(text=txt["btn"]["country"][5])
+                markup.row(btn4, btn5, btn6)
+                btn7 = types.KeyboardButton(text=txt["btn"]["country"][6])
+                btn8 = types.KeyboardButton(text=txt["btn"]["country"][7])
+                btn9 = types.KeyboardButton(text=txt["btn"]["country"][8])
+                markup.row(btn7, btn8, btn9)
+                bot.send_message(message.chat.id, text = txt["msg"]["mail"], reply_markup=markup)
+                bot.register_next_step_handler(message, send_admin_mail)
     else:
         #нейро
         if user_requests_upgrade(message) == "success":
@@ -343,10 +450,65 @@ def sub(message):
         bot.send_message(message.chat.id, text = txt["msg"]["sub_err"])
         print("meow")
         bot.register_next_step_handler(message, main_win1)
-
+    
 def send_mail(message):
     if str(message.text) in txt["btn"]["country"]:
-        id = 
-        bot.send_message(message)
+        with open("user.json", 'r+', encoding='utf-8') as user:
+            data = json.load(user)
+        bot.send_message(message.chat.id, text = txt["msg"]["mail_text"])
+        bot.register_next_step_handler(message, send_mail1, id=data[str(message.text)])
+    else:
+        bot.send_message(message.chat.id, text = txt["msg"]["country_choose2_er"])
+        bot.register_next_step_handler(message, send_mail)
 
-bot.polling(none_stop=True)
+def send_mail1(message, id):
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton(text=txt["btn"]["main_win"][0])
+    btn2 = types.KeyboardButton(text=txt["btn"]["main_win"][1])
+    btn3 = types.KeyboardButton(text=txt["btn"]["main_win"][2])
+    markup.row(btn1, btn2, btn3)
+    btn4 = types.KeyboardButton(text=txt["btn"]["main_win"][3])
+    btn5 = types.KeyboardButton(text=txt["btn"]["main_win"][4])
+    btn6 = types.KeyboardButton(text=txt["btn"]["main_win"][5])
+    markup.row(btn4, btn5, btn6)
+    bot.send_message(message.chat.id, text = txt["msg"]["mail_done"], reply_markup=markup)
+    try:
+        with open("user.json", "r", encoding='utf-8') as user:
+            user = json.load(user)
+        bot.send_message(id, text = txt["msg"]["mail_to"].format(by_user=str(user[str(message.chat.id)]["country"]), text = message.text)) 
+    except ValueError:
+        pass
+    bot.register_next_step_handler(message, main_win1)
+def news():
+    while True:
+        time.sleep(6000) 
+        send_daily_message()
+def bot_polling():
+    bot.polling(none_stop=True)   
+
+def send_daily_message():
+    with open("user.json", "r", encoding='utf-8') as user:
+        user = json.load(user)
+    for country in txt["btn"]["country"]:
+        if user[country] != 0:
+            mock_message = MockMessage(chat_id=str(user[country]), text=f"Наступил новый {user["current_data"]} год, подведи его итог и сгенерируй событие, которое положит начало новому году")
+            chat_gpt(mock_message)
+        else:
+            pass
+    mock_message = MockMessage(chat_id=user[country],text="gooooool")
+    with open("user.json", "r+", encoding='utf-8') as user:
+        data = json.load(user)
+        data["current_data"] = str(int(data["current_data"])+1)
+        user.seek(0)
+        json.dump(data, user, indent=4, ensure_ascii=False)
+        user.truncate()
+schedule.every().day.at("19:53").do(send_daily_message)
+
+if __name__ == '__main__':
+    bot_thread = threading.Thread(target=bot_polling)
+    bot_thread.start()
+    news_thread = threading.Thread(target=news)
+    news_thread.start()
+    print("чет робит")
+    while True:
+        pass
